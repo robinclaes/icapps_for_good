be5d5950fbd755b4b9fb2481ea7837cb8bb8765b
Robin Claes <robin.claes@icapps.com>
2021-06-24 14:59:09 +0200

    add mocks

---
+++ src/mocks/users.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+    helpRequests: [
+      {
+        avatar: 'https://placehold.it/160x160',
+        title: 'Breuken vermenigvuldigen',
+        description: 'Hoi, ik heb hulp nodig bij mijn huiswerk, ik ben niet zo goed in breuken vermenigvuldigen!',
+      },
+    ],
+  },
+];
